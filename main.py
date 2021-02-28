d758514395ebdae6b3d22aca319f7e26d68dbfd6
gicabral <38368320+gicabral@users.noreply.github.com>
2021-02-28 19:11:25 -0300

    Roteiro 0 feito

--- main.py
+++ main.py
@@ -8,18 +8,30 @@ def calculadora(s):
     s = list(s[::-1])
 
     #verificar se um operador não está no final ou no começo da string
-        
     if s[0] == "-" or s[0] == "+" or s[-1] == "-" or s[-1] == "+":
+        raise ValueError
+    #verifica se não foi colocado nenhum operador
+    if "+" not in s and "-" not in s:
         raise ValueError
 
     def valor():
         valor = 0
+        cont = 0
         #enquanto o caracter for um digito ele e não um operador ele fica no loop
         while s and s[-1].isdigit():
             #multiplica por 10 para pegar números compostos
             valor *= 10
             #adiciona o que saiu no pop ao valor
             valor += int(s.pop())
+        #para nao permitir casos como "1++1"
+        for i in s:
+            if i == "+" or i == "-":
+                cont += 1
+            if i.isdigit():
+                cont = 0
+            #verifica se tem dois operadores seguidos
+            if cont == 2:
+                raise ValueError
         return valor
 
     def termo():
@@ -30,12 +42,12 @@ def calculadora(s):
     result = termo()
     while s:
         op, t = s.pop(), termo()
+        #se o operador for +, realiza a soma
         if op == "+":
             result += t
-        elif op == "-":
-            result -= t
+        #senão, realiza a subtração
         else:
-            raise ValueError  
+            result -= t
     return result
 
 
